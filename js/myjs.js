815e10312aff9bfc5890a8de063ed572f32093cf
sardars270 <52626893+sardars270@users.noreply.github.com>
2022-03-07 23:48:16 -0500

    initial commit

---
+++ js/myjs.js
@@ -0,0 +1,14 @@
+document.getElementById('all').onclick.
+    document.getElementById('p').innerText = "THIS IS TEXT";
+
+function changeA() {
+    document.getElementsById("a").style.visibility = "hidden";
+}
+
+function changepB() {
+    document.getElementByClassName("b").style.display = "none";
+}
+
+function changepC() {
+    document.getElementByClassName("c").style.visibility = "hidden";
+}
